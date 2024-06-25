7151494f41811a5e3d358930b25497620e7466e5
Rushi <rushi@rushi-air.lan>
2024-06-25 15:52:44 -0700

    fix redirects

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,22 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/products/desination-reinvention-candice-denise",
+        destination: "https://www.noma-collective-bookings.com/products/desination-reinvention-candice-denise",
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: "/products/shuffle-dance-edition",
+        destination: "https://www.noma-collective-bookings.com/products/shuffle-dance-edition",
+        permanent: true,
+        basePath: false,
+      }
+    ];
+  }
 };
 
 export default nextConfig;
