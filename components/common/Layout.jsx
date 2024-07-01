60624e74d9eb1d3057aca0f2be53fd4d861e78cf
Rushi <rushi@rushi-air.lan>
2024-07-01 08:40:45 -0700

    tag manager

--- components/common/Layout.jsx
+++ components/common/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import ScrollToTopButton from "./ScrollToTopButton";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const Layout = ({ children }) => {
   return (
     <>
+    <GoogleTagManager gtmId="GTM-WRZZGKJ" />
       <Navbar />
       {children}
       <Footer />
