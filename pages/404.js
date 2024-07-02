930d876ff37616519e0b8414d556697b61816723
Rushi <rushi@rushi-air.lan>
2024-07-02 07:45:31 -0700

    reouter replace

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+// pages/404.js
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const Custom404 = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  }, [router]);
+
+  return null;
+};
+
+export default Custom404;
