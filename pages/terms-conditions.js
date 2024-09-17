0996e4622e1f1dad3f9e3494f70eb5d5cd124bad
chris0926 <chris@noma-collective.com>
2024-09-17 13:53:24 -0400

    Update terms-conditions.js

--- pages/terms-conditions.js
+++ pages/terms-conditions.js
@@ -159,6 +159,17 @@ const TermsConditions = () => {
           on to you in good faith.
         </p>
 
+        <h2>Refunds and Dispute Resolution with Location Partners</h2>
+        <p>
+          In the event of a dispute with our location partner 
+          (e.g., accommodation provider), we will not be liable for issuing 
+          refunds until the dispute has been fully resolved. Refunds will 
+          only be processed once we have received the corresponding funds back 
+          from the location partner. We will make every reasonable effort to 
+          expedite the resolution process, but delays may occur based on the 
+          policies of the location partner involved.
+        </p>
+
         <h2>Visa, Passport and Health Requirements</h2>
         <p>
           It is your own responsibility to ensure compliance with immigration,
